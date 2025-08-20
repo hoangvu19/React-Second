f03665e9046527d5da8ea6b8c71b12507c58b8bc
hoangvu19 <114670059+hoangvu19@users.noreply.github.com>
2025-08-20 13:16:19 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProjectUser from './components/ProjectUser';
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello Work</h1>
+      <ProjectUser />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ProjectUser.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import anh from "../assets/img/Bao.jpg";
+interface User  {
+	id:string;
+	name: string;
+	email: string;
+	avatarUrl?: string;
+};
+
+
+const ProjectUser = () => {
+	const [user, setUser] = React.useState<User | null>(null);
+	const [loading, setLoading] = React.useState(true);
+
+	React.useEffect(() => {
+		setTimeout(() => {
+			setUser({
+				id: "1",
+				name: "Phạm Đức Hoàng Vũ",
+				email: "vupham.190504@gmail.com",
+				avatarUrl: anh
+			});
+			setLoading(false);
+		}, 2000);
+	}, []);
+
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+	if (!user) return null;
+	return (
+		<div className="user-card">
+			<img
+				src={user.avatarUrl || anh}
+				alt={user.name}
+			/>
+			<div>
+				<h3>{user.name}</h3>
+				<p>{user.email}</p>
+			</div>
+		</div>
+	);
+};
+
+export default ProjectUser;
